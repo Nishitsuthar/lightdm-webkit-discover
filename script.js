9e4a2055142bdb8661a96e288c3e7bdab55a3a46
nishitsuthar <nishitsuthar123@gmail.com>
2021-09-19 11:47:44 +0530

    mock file edited

---
+++ script.js
@@ -0,0 +1,81 @@
+let time_remaining = 0;
+let selected_user = null;
+let valid_image = /.*\.(png|svg|jpg|jpeg|bmp)$/i;
+
+// when greeter ask for login prompt this method called 
+function show_prompt(text){
+
+}
+
+// called when greeter ask for showing message 
+function show_message(text){
+
+}
+
+// called when greeter ask for error 
+function show_error(text){
+
+}
+
+// called when greeter complete authentication request 
+function authentication_complete(){
+
+}
+
+// called then greeter wants to timed login 
+function timed_login(user){
+
+}
+
+// implemation of authentication starts from here 
+function start_authentication(username){
+
+}
+
+function provide_secret(){
+
+}
+
+function initialize_sessions(){
+
+}
+
+function show_users(){
+
+}
+
+function user_clicked(event){
+
+}
+
+function setVisible(element, visible){
+
+}
+
+function isVisible(element){
+
+}
+
+function update_time(){
+
+}
+
+function initialize(){
+
+}
+
+function on_image_error(e){
+
+}
+
+function initialize_users(){
+
+}
+
+function initialize_timer(){
+
+}
+
+function add_action(id, name, image, clickhander, template, parent){
+    
+}
